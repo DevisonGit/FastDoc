54787986ce6fad849d7c79add0b9c78c62f1dc11
devisono <devison_santos@yahoo.com.br>
2025-06-28 19:44:13 -0300

    parametros de consulta

--- main.py
+++ main.py
@@ -1,45 +1,76 @@
-from enum import Enum
+from typing import Union
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-class ModelName(str, Enum):
-    alexnet = 'alexnet'
-    resnet = 'resnet'
-    lenet = 'lenet'
+fake_items_db = [
+    {'item_name': 'foo'}, {'item_name': 'bar'}, {'item_name': 'baz'}
+]
 
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello World'}
+@app.get('/items/')
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
 
 
 @app.get('/items/{item_id}')
-async def read_item(item_id: int):
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {'item_id': item_id, 'q': q}
     return {'item_id': item_id}
 
 
-@app.get('/users/me')
-async def read_user_me():
-    return {'user_id': 'the current user'}
+@app.get('/union/items/{item_id}')
+async def read_item_union(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {'item_id': item_id, 'q': q}
+    return {'item_id': item_id}
 
 
-@app.get('/users/{user_id}')
-async def read_user(user_id: int):
-    return {'user_id': user_id}
+@app.get('/bool/items/{item_id}')
+async def read_item_bool(
+        item_id: str, q: str | None = None, short: bool = False
+):
+    item = {'item_id': item_id}
+    if q:
+        item.update({'q': q})
+    if not short:
+        item.update(
+            {
+                'description':
+                    'this is a amazing item that has a long description'
+            }
+        )
+    return item
 
 
-@app.get('/model/{model_name}')
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {'model_name': model_name, 'message': 'Deep Learning FTW!'}
-    if model_name.value == 'lenet':
-        return {'model_name': model_name, 'massage': 'LeCNN all the images'}
-    return {'model_name': model_name, 'message': 'Have some residuals'}
+@app.get('/users/{user_id}/items/{item_id}')
+async def read_user_item(
+        user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {'item_id': item_id ,'owner_id': user_id}
+    if q:
+        item.update({'q': q})
+    if not short:
+        item.update(
+            {
+                'description':
+                    'this is a amazing item that has a long description'
+            }
+        )
+    return item
 
 
-@app.get('/files/{file_path:path}')
-async def read_file(file_path: str):
-    return {'file_path': file_path}
+@app.get('/needy/items/{item_id}')
+async def read_user_item(item_id: str, needy: str):
+    item = {'item_id': item_id, 'needy': needy}
+    return item
+
+
+@app.get('/mixer/items/{item_id}')
+async def read_user_item(
+        item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {'item_id': item_id, 'needy': needy, 'skip': skip, 'limit': limit}
+    return item
